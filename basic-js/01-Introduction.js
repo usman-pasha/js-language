5534555c5bb1833777527fcd85e7070dc66f9d7a
usman pasha <usman@evvoiot.com>
2023-01-19 15:54:19 +0530

    intro added

---
+++ basic-js/01-Introduction.js
@@ -0,0 +1,4 @@
+`JavaScript was initially created to “make web pages alive”.
+The programs in this language are called scripts. They can be written right in a web page’s HTML and run automatically as the page loads.
+Scripts are provided and executed as plain text. They don’t need special preparation or compilation to run.
+In this aspect, JavaScript is very different from another language called Java.`
